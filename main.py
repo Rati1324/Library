ac529c92fb21a9cd5b23422e54316269640f96d1
rati1324 <rati.mtsituri@gau.edu.ge>
2023-09-26 09:03:24 +0400

    added user signup

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, HTTPException, Depends
 from src.config import Base, engine, SessionLocal
 from src.schemas import UserSchema
-import src.models as models
+from src.models import User
 from sqlalchemy.orm import Session
+from src.utils import get_hashed_password
 
 app = FastAPI()
 
@@ -21,12 +22,31 @@ def greet():
 
 @app.get("/users")
 async def get_users(db: Session = Depends(get_db)):
-    return db.query(models.User).all()
+    return db.query(User).all()
 
 @app.post("/users")
 async def create_user(user: UserSchema, db: Session = Depends(get_db)):
-    db_user = models.User(username=user.username, email=user.email, password=user.password)
+    db_user = User(username=user.username, email=user.email, password=user.password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+@app.post("/signup")
+async def signup(db: Session = Depends(get_db), user_data: UserSchema = None):
+    # find user in the db based on user_data.email
+    user = db.query(User).filter_by(email=user_data.email).first()
+    if user is not None:
+        raise HTTPException(status_code=400, detail="Email already registered")
+
+    db_user = User(
+        username=user_data.username, 
+        email=user_data.email, 
+        password=get_hashed_password(user_data.password)
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return {"result": "user successfully created"}
--- src/schemas.py
+++ src/schemas.py
@@ -1,6 +1,5 @@
 from typing import List, Optional, Generic, TypeVar
 from pydantic import BaseModel , Field
-from pydantic.generics import GenericModel
 
 # T = TypeVar('T')
 
