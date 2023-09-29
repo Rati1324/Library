f0cd8419e37eb4ddc0238c2dab028f831cf449e9
rati1324 <rati.mtsituri@gau.edu.ge>
2023-09-29 09:02:23 +0400

    _

--- src/utils2.py
+++ src/utils2.py
@@ -47,3 +47,24 @@ def decode_jwt(token: str):
     return decode_token if decode_token["exp"] >= time.time() else False
     # except:
     #     return {}
+
+# receives token and decodes it, returns user
+async def get_current_user(token: str = Depends(oauth_2_scheme)):
+    credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credential_exception
+       
+        token_data = TokenData(username=username)
+
+    except JWTError:
+        raise credential_exception
+    
+    user = get_user(db, username=token_data.username)
+    if user is None:
+        raise credential_exception
+       
+    return user
